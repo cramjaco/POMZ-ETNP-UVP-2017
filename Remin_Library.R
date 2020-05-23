0283c626915e75775114d8c41d0cc893475d16b1
Jacob A Cram <jcram@umces.edu>
2020-05-22 21:12:42 -0400

    Non linear remineralization model

--- Remin_Library.R
+++ Remin_Library.R
@@ -1,5 +1,14 @@
+library(tidyverse)
+
 ## Variables
 
+lb_vec <- c(0.102, 0.128, 0.161, 0.203, 0.256, 0.323, 0.406, 0.512, 0.645, 
+            0.813, 1.02, 1.29, 1.63, 2.05, 2.58, 3.25, 4.1, 5.16, 6.5, 8.19, 
+            10.3, 13, 16.4, 20.6, 26)
+
+binsize_vec <- c(0.026, 0.033, 0.042, 0.053, 0.067, 0.083, 0.106, 0.133, 0.168, 
+                 0.207, 0.27, 0.34, 0.42, 0.53, 0.67, 0.85, 1.06, 1.34, 1.69, 
+                 2.11, 2.7, 3.4, 4.2, 5.4, 6)
 
 # mass of a 1mm particle
 m1mm = 3.3 * 10^-6; #%g % Alldgedge 1998 % mass of 1mm particle
@@ -10,24 +19,28 @@ micron = 1e-6;
 #ag = 0.26
 
 # So we can conform to usual expectations of alpha and gamma being positive
-C_f = C_f # defined earlier
-ag = ag # defined earlier
+C_f = 10.51 # for now
+C_f_global = C_f
+alpha = .52
+gamma = 0.26
+ag = alpha + gamma
+ag_global = ag
 
 Cm = m1mm
 Cw = w1mm
 m_vec =  Cm * lb_vec ^ alpha;
 w_vec = Cw * lb_vec ^ gamma;
 
-test_abun_in <- synthetic_data %>% filter(depth == 25) %>% pull(spec) %>%.[[1]] %>% pull(np)
+# test_abun_in <- synthetic_data %>% filter(depth == 25) %>% pull(spec) %>%.[[1]] %>% pull(np)
 
-f_vec0 <- test_abun_in * m_vec * w_vec
-F0 <- sum(f_vec0)
+# f_vec0 <- test_abun_in * m_vec * w_vec
+# F0 <- sum(f_vec0)
 
-test_abun_out <- synthetic_data %>% filter(depth == 50) %>% pull(spec) %>% .[[1]] %>% pull(np)
-f_vec1 <- test_abun_out * m_vec * w_vec
-F1 <- sum(f_vec1)
-DFP <- F1/F0
-DFP
+# test_abun_out <- synthetic_data %>% filter(depth == 50) %>% pull(spec) %>% .[[1]] %>% pull(np)
+# f_vec1 <- test_abun_out * m_vec * w_vec
+# F1 <- sum(f_vec1)
+# DFP <- F1/F0
+# DFP
 
 little_lb <- lb_vec[1] - (lb_vec[2] - lb_vec[1])/2 # size of the particle that the UVP can't see anymore. Eg, things actually shrink to this size but then they vanish from the UVP's view. Just leting it be like, the difference in size of the smallest two bins smaller than the smallest bin.
 
@@ -120,5 +133,10 @@ shuffle_tune <- function(DFpct_toRemin, abun_in,  DFpct_target,...){
   rmse
 }
 
-stOpt <- optimize(shuffle_tune, c(0, 2), abun_in = test_abun_in, DFpct_target = 0.9)
-stOpt
+# stOpt <- optimize(shuffle_tune, c(0, 2), abun_in = test_abun_in, DFpct_target = 0.9)
+# stOpt
+
+optFun <- function(abun_in, DFpct){
+  opt <- optimize(shuffle_tune, c(0, 2), abun_in = abun_in, DFpct_target = DFpct)
+  opt$minimum
+}
